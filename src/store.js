973dd3fbeae10a2e69b7f3f97c37444f7084c73e
DevsignerUnicorn <mzapien91@hotmail.com>
2025-05-25 15:00:43 -0400

    Confirm Task 5: Redux store correctly configured

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
+
+// Create a Redux store using configurationStore from Redux Toolkit
+const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: cartReducer, // 'cart' slice is managed by cartReducer
     },
 });
-export default store
+export default store;  // Export for use in <Provider store={store}>
